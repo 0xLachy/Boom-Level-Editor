e0f0cd6ea519f70d1f23a78ce9c402a8b196ca52
LachyLegend <lachylegend05@gmail.com>
2022-07-24 08:44:21 +1000

    plhs generator adds custom level name property now

---
+++ Assets/Scripts/SpriteFlipRotater.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Make it run in the Editor 
+public class SpriteFlipRotater : MonoBehaviour
+{    
+    //When you right click on a game object, an option comes up that says flip sprite
+    //If the object name contains flat 180 - current angle
+    //main kind of curve 270 - currentAngle
+    //that small curve is 315 - current angle (I think)
+
+    //more stuff like that
+
+}
